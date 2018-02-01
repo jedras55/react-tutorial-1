ae53e8b39cc3e3ced4c009c5da6a28c227d3fa28
jedras55 <jedrzej.ostrowski@gmail.com>
2018-02-01 01:41:49 +0100

    Class component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyB2E5kwwSZTxEv9b8JeyZ24Np-crEcm8v4';
 
 // Create a new component. This component should procude
 // some HTML
 const App = () =>{
-    return <div>Hi!</div>;
-}
+   return (
+       <div>
+           <SearchBar/>
+       </div>
+   );
+};
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
